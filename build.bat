dab2d0fdf108d5312641f9f04c308ae45592b8d2
rafa1231518 <chris331hansen@outlook.com>
2016-05-02 18:38:36 -0300

    add errorlevel checks for build.bat

--- build.bat
+++ build.bat
@@ -5,14 +5,28 @@ echo X                                                 X
 echo X                                                 X
 
 g++ filechars.cpp -o filechars.exe
+if not errorlevel 0 (
+	pause
+	exit
+)
+
 filechars readme.txt
 del filechars.exe
 
 cd include\minizip
 gcc -c -DWIN32=1 -I.\include -I.. minizip.c miniunz.c zip.c unzip.c iowin32.c ioapi.c
+if not errorlevel 0 (
+	pause
+	exit
+)
+
 cd ..\..
 
 g++ -Wall -DMSW=1 unwad.cpp qmus2mid.cpp -I./include -I./include/pngpp -Ldll -lmagic1 -lzlib1 -lregex2 -llibpng13  -o unwad.exe
+if not errorlevel 0 (
+	pause
+	exit
+)
 
 cd include\minizip
 del *.o
