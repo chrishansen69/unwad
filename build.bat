82e77e51f12c189649c9cf0a681756c77bb8e079
rafa1231518 <chris331hansen@outlook.com>
2016-05-02 18:44:55 -0300

    final checks + exit 1

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ echo X                                                 X
 g++ filechars.cpp -o filechars.exe
 if not errorlevel 0 (
 	pause
-	exit
+	exit 1
 )
 
 filechars readme.txt
@@ -17,7 +17,7 @@ cd include\minizip
 gcc -c -DWIN32=1 -I.\include -I.. minizip.c miniunz.c zip.c unzip.c iowin32.c ioapi.c
 if not errorlevel 0 (
 	pause
-	exit
+	exit 1
 )
 
 cd ..\..
@@ -25,9 +25,11 @@ cd ..\..
 g++ -Wall -DMSW=1 unwad.cpp qmus2mid.cpp -I./include -I./include/pngpp -Ldll -lmagic1 -lzlib1 -lregex2 -llibpng13  -o unwad.exe
 if not errorlevel 0 (
 	pause
-	exit
+	exit 1
 )
 
 cd include\minizip
 del *.o
 cd ..\..
+
+if not exist unwad.exe exit 1
