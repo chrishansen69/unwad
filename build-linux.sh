fd6690e1d119468e152893ecb9c19ee97bda3ed3
rafa1231518 <chris331hansen@outlook.com>
2016-05-02 21:01:06 -0300

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -10,7 +10,7 @@ minizip.c miniunz.c zip.c unzip.c ioapi.c
 cd ../..
 
 g++ -Wall \
--L./lib
+-L./lib \
 -I./include/pngpp -I./include \
 unwad.cpp qmus2mid.cpp \
 include/minizip/minizip.o include/minizip/ioapi.o include/minizip/zip.o include/minizip/unzip.o \
@@ -22,7 +22,7 @@ strip unwad
 echo building unwad.static
 
 g++ -Wall \
--L./lib
+-L./lib \
 -I./include/pngpp -I./include \
 unwad.cpp qmus2mid.cpp \
 include/minizip/minizip.o include/minizip/ioapi.o include/minizip/zip.o include/minizip/unzip.o \
